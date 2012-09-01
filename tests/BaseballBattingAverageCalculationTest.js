e9435805e10eb78e80803d9071cc402da7ea9600
Nariki Yasutaka <prototype099@gmail.com>
2012-09-01 10:51:34 +0900

    base test file added

---
+++ tests/BaseballBattingAverageCalculationTest.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+
+	module("Part 1");
+
+	test("sample test", function(){
+		equal(true, true, "this is sample test.");
+	});
+
+	/*
+	test("first test within module", function() {
+	  ok( true, "all pass" );
+	});
+
+	test("second test within module", function() {
+	  ok( true, "all pass" );
+	});
+
+	module("Module B");
+
+	test("some other test", function() {
+	  expect(2);
+	  equal( true, false, "failing test" );
+	  equal( true, true, "passing test" );
+	});
+
+	*/
+
+});
