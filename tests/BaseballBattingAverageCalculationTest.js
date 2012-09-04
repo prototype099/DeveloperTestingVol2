6a3ef5eff7154f93442677a5e5ec9c80ea381719
Nariki Yasutaka <prototype099@gmail.com>
2012-09-04 13:57:51 +0900

    part1 finished

--- tests/BaseballBattingAverageCalculationTest.js
+++ tests/BaseballBattingAverageCalculationTest.js
@@ -4,27 +4,16 @@ $(document).ready(function(){
 
 	module("Part 1");
 
-	test("sample test", function(){
-		equal(true, true, "this is sample test.");
+	test("打率が計算できる（打率=安打数/打数）", function(){
+		equal( baseball.calcBattingAverage(3,2,1), "0.500", "3打席2打数1安打 = 打率0.500");
 	});
 
-	/*
-	test("first test within module", function() {
-	  ok( true, "all pass" );
+	test("打率は小数第四位で四捨五入される（切り捨てる場合）", function(){
+		equal(baseball.calcBattingAverage(3,3,1), "0.333", "3打席3打数1安打 = 打率0.333");
 	});
 
-	test("second test within module", function() {
-	  ok( true, "all pass" );
+	test("打率は小数第四位で四捨五入される(切り上げる場合)", function(){
+		equal(baseball.calcBattingAverage(3,3,2), "0.667", "3打席3打数2安打 = 打率0.667");
 	});
-
-	module("Module B");
-
-	test("some other test", function() {
-	  expect(2);
-	  equal( true, false, "failing test" );
-	  equal( true, true, "passing test" );
-	});
-
-	*/
 
 });
