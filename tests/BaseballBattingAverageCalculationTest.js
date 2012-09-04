c9ba343ea479f86311bdf01008821e678a8a05c5
Nariki Yasutaka <prototype099@gmail.com>
2012-09-04 14:40:00 +0900

    part3 finished

--- tests/BaseballBattingAverageCalculationTest.js
+++ tests/BaseballBattingAverageCalculationTest.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 	// Part 1
 	test("打率が計算できる（打率=安打数/打数）", function(){
 		equal( baseball.calcBattingAverage(3,2,1), "0.500", "3打席2打数1安打 = 打率0.500");
+		equal( baseball.calcBattingAverage(3,2,0), "0.000", "3打席2打数0安打 = 打率0.000");
 	});
 
 	test("打率は小数第四位で四捨五入される（切り捨てる場合）", function(){
@@ -17,8 +18,19 @@ $(document).ready(function(){
 		equal(baseball.calcBattingAverage(3,3,2), "0.667", "3打席3打数2安打 = 打率0.667");
 	});
 
+
 	// Part2
 	test("打席数が0の場合、打率計算しない（undefined）", function(){
-		equal(baseball.calcBattingAverage(0,1,1), undefined, "打席数が0のときはundefinedを返す")
+		equal(baseball.calcBattingAverage(0,1,1), undefined, "打席数が0のときはundefinedを返す");
 	});
+
+
+	// Part3
+	test("打席数が0でなく打数が0の場合は0.000と計算", function(){
+		equal(baseball.calcBattingAverage(3,0,0), "0.000", "打席数が0でなく打数が0の場合 = 0.000");
+		equal(baseball.calcBattingAverage(3,0,1), "0.000", "打席数が0でなく打数が0の場合2 = 0.000");
+	});
+
+	
+
 });
