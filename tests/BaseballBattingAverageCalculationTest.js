d97eea4ee9bc0506bec7fa8d983c16acc6e00060
Nariki Yasutaka <prototype099@gmail.com>
2012-09-04 14:24:18 +0900

    part2 finished

--- tests/BaseballBattingAverageCalculationTest.js
+++ tests/BaseballBattingAverageCalculationTest.js
@@ -2,8 +2,9 @@
 
 $(document).ready(function(){
 
-	module("Part 1");
+	module("BaseballBattingAverageCalculationTest.js");
 
+	// Part 1
 	test("打率が計算できる（打率=安打数/打数）", function(){
 		equal( baseball.calcBattingAverage(3,2,1), "0.500", "3打席2打数1安打 = 打率0.500");
 	});
@@ -16,4 +17,8 @@ $(document).ready(function(){
 		equal(baseball.calcBattingAverage(3,3,2), "0.667", "3打席3打数2安打 = 打率0.667");
 	});
 
+	// Part2
+	test("打席数が0の場合、打率計算しない（undefined）", function(){
+		equal(baseball.calcBattingAverage(0,1,1), undefined, "打席数が0のときはundefinedを返す")
+	});
 });
